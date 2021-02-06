9d3c906a20af9823e1560c3c1ba6ec71954ef375
bilalaoutah <aoutahbilal372@gmail.com>
2021-02-06 11:33:12 +0100

    here is first commit

---
+++ src/main/java/ma/projet/timemanagement/service/UserService.java
@@ -0,0 +1,16 @@
+package ma.projet.timemanagement.service;
+
+import ma.projet.timemanagement.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+@Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+}
